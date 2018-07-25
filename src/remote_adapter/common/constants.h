ddf1dd311df15ca2655dfa3309757013e7ad2429
sniukalov <sniukalov@luxoft.com>
2018-07-25 20:33:00 +0300

    UtilsManager: Updated base functionality.

--- src/remote_adapter/common/constants.h
+++ src/remote_adapter/common/constants.h
@@ -21,18 +21,30 @@ static std::string app_check_status = "app_check_status";
 static std::string file_backup = "file_backup";
 static std::string file_restore = "file_restore";
 static std::string file_update = "file_update";
+static std::string file_exists = "file_exists"; 
+static std::string file_content = "file_content";
+static std::string file_delete = "file_delete";
+static std::string folder_exists = "folder_exists";
+static std::string folder_delete = "folder_delete";
+
+namespace stat_app_codes{
+static const int CRASHED = -1;
+static const int NOT_RUNNING = 0;
+static const int RUNNING = 1;
+};
 
 
 namespace error_codes {
 static const int SUCCESS = 0;
-static const int READ_FAILURE = -1;
-static const int WRITE_FAILURE = -2;
-static const int PATH_NOT_FOUND = -3;
-static const int CLOSE_FAILURE = -4;
-static const int OPEN_FAILURE = -5;
-static const int NO_CONNECTION = -6;
-static const int EXCEPTION_THROWN = -7;
-static const int TIMEOUT_EXPIRED = -8;
-static const int ALREADY_EXISTS = -9;
+static const int FAILED  = -1;
+static const int READ_FAILURE = -2;
+static const int WRITE_FAILURE = -3;
+static const int PATH_NOT_FOUND = -4;
+static const int CLOSE_FAILURE = -5;
+static const int OPEN_FAILURE = -6;
+static const int NO_CONNECTION = -7;
+static const int EXCEPTION_THROWN = -8;
+static const int TIMEOUT_EXPIRED = -9;
+static const int ALREADY_EXISTS = -10;
 }  // namespace error_codes
 }  // namespace constants
--- src/remote_adapter/remote_adapter_server/utils_manager.h
+++ src/remote_adapter/remote_adapter_server/utils_manager.h
@@ -1,17 +1,32 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
+#include <sys/procfs.h>
+#include <signal.h>
 namespace utils_wrappers {
 
 class UtilsManager {
  public:
-    int AppStart(const std::string & app_path,const std::string & app_name);
-    int AppStop(const std::string & app_name);
-    int AppCheckStatus(const std::string & app_name);
-    int FileBackup(const std::string & file_path,const std::string & file_name);
-    int FileRestore(const std::string & file_path,const std::string & file_name);
-    int FileUpdate(const std::string & file_path,const std::string & file_name,const std::string & file_content);
+ 	typedef std::vector<int> ArrayPid;
+
+    static int StartApp(const std::string & app_path,const std::string & app_name);
+    static int StopApp(const std::string & app_name,const int sig = SIGTERM);
+    static int CheckStatusApp(const std::string & app_name);
+    static int FileBackup(const std::string & file_path,const std::string & file_name);
+    static int FileRestore(const std::string & file_path,const std::string & file_name);
+    static int FileUpdate(const std::string & file_path,const std::string & file_name,const std::string & file_content);
+    static int FileExists(const std::string & file_path,const std::string & file_name);
+    static int FileDelete(const std::string & file_path,const std::string & file_name);
+    static std::string GetFileContent(const std::string & file_path,const std::string & file_name);
+    static int FolderExists(const std::string & folder_path,const std::string & folder_name);
+    static int FolderDelete(const std::string & folder_path);
+
+ private:
+    static ArrayPid GetPidApp(const std::string & app_name);
+    static std::string GetNameApp(int pid,procfs_info * proc_info = 0);
+    static int KillApp(const pid_t app_pid,const int sig,const char * app_name = 0);
+    static bool IsExistsApp(const pid_t app_pid);
  };
 
 }  // namespace utils_wrappers
